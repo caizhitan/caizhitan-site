c6b5efd1fa07efbf3e9678714e480a176207d514
caizhitan <caizhi@kailabs.art>
2026-02-10 09:56:05 +0800

    feat: lineis implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Lenis from 'lenis'
 import LandingPage2 from './Pages/LandingPage2'
 
 function App() {
+  useEffect(() => {
+    const lenis = new Lenis()
+
+    function raf(time) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+
+    requestAnimationFrame(raf)
+
+    return () => {
+      lenis.destroy()
+    }
+  }, [])
+
   return (
     <>
       <LandingPage2 />
